f7f46d494a20cfcb613816cc5e66e3f7d5ca20bd
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2025-09-09 13:02:15 -0500

    Initial Commit

---
+++ AtlasPDA/AtlasPDAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasPDAApp.swift
+//  AtlasPDA
+//
+//  Created by Matthew Dudzinski on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlasPDAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
